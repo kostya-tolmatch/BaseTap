7c18890e8a711b624f4d7fd2b524b86486632cf4
kostya-tolmatch <kostyatolmatch@outlook.com>
2025-08-23 22:11:04 +0000

    feat: add factory deployment script

---
+++ script/DeployFactory.s.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.23;
+
+import "forge-std/Script.sol";
+import "../src/TapFactory.sol";
+
+contract DeployFactory is Script {
+    function run() external returns (address) {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        
+        vm.startBroadcast(deployerPrivateKey);
+        
+        TapFactory factory = new TapFactory();
+        console.log("TapFactory deployed at:", address(factory));
+        
+        vm.stopBroadcast();
+        
+        return address(factory);
+    }
+}
